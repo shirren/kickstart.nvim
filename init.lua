94dfbefa0e49036f8ec8fab7ef82981492382710
Shirren Premaratne <shirren@me.com>
2025-06-11 21:13:22 +1000

    Added transparent background to catppuccin theme

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -886,7 +886,11 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('catppuccin').setup {
+        transparent_background = true,
         styles = {},
+        integrations = {
+          nvimtree = true,
+        },
       }
 
       -- Load the colorscheme here.
